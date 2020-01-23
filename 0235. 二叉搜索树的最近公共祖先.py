c9aea305d94eeacb610e411fb21bb6b1a6ebf543
surika <526235290@qq.com>
2020-01-23 23:17:24 +0800

    2020.1.23

---
+++ 0235. 二叉搜索树的最近公共祖先.py
@@ -0,0 +1,33 @@
+'''
+解题思路
+
+根结点比两个结点都大 就在左子树找
+根结点比两个结点都小 就在右子树找
+否则就返回根结点
+'''
+class Solution:
+    def lowestCommonAncestor(self, root, p, q):
+        """
+        :type root: TreeNode
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: TreeNode
+        """
+        # Value of current node or parent node.
+        parent_val = root.val
+
+        # Value of p
+        p_val = p.val
+
+        # Value of q
+        q_val = q.val
+
+        # If both p and q are greater than parent
+        if p_val > parent_val and q_val > parent_val:    
+            return self.lowestCommonAncestor(root.right, p, q)
+        # If both p and q are lesser than parent
+        elif p_val < parent_val and q_val < parent_val:    
+            return self.lowestCommonAncestor(root.left, p, q)
+        # We have found the split point, i.e. the LCA node.
+        else:
+            return root
